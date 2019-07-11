cfc807d7832263a6744b21131f4ed9f8151d66f6
user95f85f <48297795+user95f85f@users.noreply.github.com>
2019-07-11 16:36:37 -0700

    Update create-do-programming.pl

--- create-do-programming.pl
+++ create-do-programming.pl
@@ -3,15 +3,31 @@
 use warnings;
 use strict;
 use feature qw/say/;
+use List::Util qw/shuffle/;
 
 open my $f, '>', 'out.html' or die $!;
 
+my $t =<<'EOF2';
+<button onclick="go('b').innerHTML=this.innerHTML">be a python guru</button><br />
+<button onclick="go('b').innerHTML=this.innerHTML">be a restricted-python guru</button><br />
+<button onclick="go('b').innerHTML=this.innerHTML">be a C++ guru</button><br />
+<button onclick="go('b').innerHTML=this.innerHTML">be a C guru</button><br />
+<button onclick="go('b').innerHTML=this.innerHTML">be a C expert guru</button><br />
+<button onclick="go('b').innerHTML=this.innerHTML">be an HTML5/HTML guru</button><br />
+<button onclick="go('b').innerHTML=this.innerHTML">be a Go guru</button><br />
+<button onclick="go('b').innerHTML=this.innerHTML">be a Unity3d guru</button><br />
+<button onclick="go('b').innerHTML=this.innerHTML">be a web perl guru</button><br />
+<button onclick="go('b').innerHTML=this.innerHTML">be a node.js guru</button><br />
+<button onclick="go('b').innerHTML=this.innerHTML">be an assembly expert guru</button><br />
+EOF2
+my $x = join("\n", shuffle(split /\n/, $t));
 
-say $f <<'EOF';
+
+say $f <<'EOF3';
 <!DOCTYPE html>
 <html lang="en">
 <head><meta charset="utf-8" />
-<title>learning</title>
+<title>do programming</title>
 <script>
 var d = document;
 function go(_s){
@@ -28,15 +44,14 @@ body{font-size:1.2em}
 <body onload="body_onloaded();">
 <table>
 <tr><td id="b">
-<button onclick="go('b').innerHTML='be a python guru'">be a python guru</button><br />
-<button onclick="go('b').innerHTML='be a C++ guru'">be a C++ guru</button><br />
-<button onclick="go('b').innerHTML='be a C guru'">be a C guru</button><br />
-<button onclick="go('b').innerHTML='be an HTML5/HTML guru'">be an HTML5/HTML guru</button><br />
+EOF3
+say $f $x;
+say $f <<'EOF4';
 </td><td>
 <a href="#" onclick="this.innerHTML = 'do assignment ' + ++i;return false">do assignment 1</a>
 </td></tr>
 </table>
 </body>
 </html>
-EOF
+EOF4
 close($f);
